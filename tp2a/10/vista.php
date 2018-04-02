ce8ec4b870b35ba55f9b9e02b66ebddc1ec872c5
leomarazzo <leonardojmarazzo@gmail.com>
2018-04-02 20:55:02 -0300

    Ejercicio 10

---
+++ tp2a/10/vista.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Upload</title>
+</head>
+<body>
+	<?php if ( $res == true ): ?>
+		<h1>El archivo <?=${$archivo}?> ha sido almacenado</h1>
+	<?php endif ?>
+</body>
+</html>
