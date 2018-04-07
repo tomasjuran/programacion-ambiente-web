25a63d2b8c0b26e208d59084673ef99d69941c34
root <tomasjuran96@gmail.com>
2018-04-07 00:18:42 -0300

    terminado tp2

---
+++ tp2a/12/cabeza.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="es-AR">
+<head>
+	<meta charset="UTF-8">
+	<title><?=${title}?></title>
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+</head>
+<body>
+	
---
+++ tp2a/12/post.php
@@ -0,0 +1,13 @@
+<article>
+	<form action="<?= ${accion} ?>" method="post" enctype="multipart/form-data">
+		<input type="hidden" name="idpost" value="<?= ${idpost} ?>">
+		<label for="titulo">Título</label>
+		<input type="text" name="titulo" id="titulo" value="<?= ${titulo} ?>" maxlength="200" placeholder="Ingrese el título">
+		<label for="cuerpo">Mensaje</label>
+		<textarea name="cuerpo" id="cuerpo" maxlength="1000"><?= ${cuerpo} ?></textarea>
+		<label for="imagen_up">Subir una imagen</label>
+		<input type="file" name="imagen_up" id="imagen_up">
+		<input type="submit" name="publicar" value="Publicar">
+	</form>
+	<img src="<?= ${imagen} ?>" alt="Imagen">
+</article>
