95de170895a34b85eaa33215f65745baf5f48cd9
root <tomasjuran96@gmail.com>
2018-04-07 00:16:14 -0300

    terminado tp2

--- tp2a/12/cabeza.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="es-AR">
-<head>
-	<meta charset="UTF-8">
-	<title><?=${title}?></title>
-	<link rel="stylesheet" type="text/css" href="styles/style.css">
-</head>
-<body>
-	
--- tp2a/12/vista_editar.php
+++
@@ -1,7 +0,0 @@
-<nav>
-	<a href="index.php">Volver a la página principal</a>
-</nav>
-<section>
-	<p><?=${resultado_post}?></p>
-	<p><?=${error_imagen}?></p>
-</section>
